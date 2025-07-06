ce2b13338670c94cf27d9b928e5decbb2efe5c68
ewellyn <itsewellyn@gmail.com>
2025-07-06 14:03:43 -0300

    feat: fazendo modal de adicionar nova impressora

--- printer-management-system/frontend/src/app/layout.tsx
+++ printer-management-system/frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import QueryProvider from '../components/QueryProvider';
+import { Toaster } from "../components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={inter.className}>
         <QueryProvider>{children}</QueryProvider>
+        <Toaster richColors />
       </body>
     </html>
   );
