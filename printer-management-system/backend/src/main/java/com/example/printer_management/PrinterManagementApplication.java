aaa23f315a886c9483e11c7b6d88ac4466830372
ewellyn <itsewellyn@gmail.com>
2025-07-06 21:28:50 -0300

    feat: setup inicial backend do projeto com Spring Boot e Maven

---
+++ printer-management-system/backend/src/main/java/com/example/printer_management/PrinterManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.printer_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinterManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinterManagementApplication.class, args);
+	}
+
+}
